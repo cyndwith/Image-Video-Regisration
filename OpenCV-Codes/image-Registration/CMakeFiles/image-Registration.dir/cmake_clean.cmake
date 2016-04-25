18154a76533392c8ea25c332af019a2415249ba3
cyndwith@gmail.com <cyndwith@gmail.com>
2016-04-25 01:17:30 -0400

    image-Registration folder added

---
+++ OpenCV-Codes/image-Registration/CMakeFiles/image-Registration.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/image-Registration.dir/stitching.cpp.o"
+  "image-Registration.pdb"
+  "image-Registration"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/image-Registration.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
